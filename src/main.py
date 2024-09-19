45fbcac954f2b9fe1be2382f0d7f2f87860b9586
tony-cannibal <castrovital.luis@gmail.com>
2024-09-18 20:42:15 -0600

    Finished Core Logic

--- src/main.py
+++ src/main.py
@@ -16,14 +16,23 @@ class Application(ttk.Frame):
         self.grid_columnconfigure(
             (0, 1, 2, 3, 4, 5), weight=1, uniform="column")
 
-        self.captura = ttk.Entry(self)
-        self.captura.grid(row=0, column=0, columnspan=3)
+        self.captura = ttk.Entry(self, justify='center')
+        self.captura.grid(row=0, column=0, columnspan=6, sticky="EW")
 
         self.np_number = ttk.Label(self, text="0")
-        self.np_number.grid(row=1, column=3, columnspan=3)
+        self.np_number.grid(row=2, column=3, columnspan=3)
 
         self.num_parte = ttk.Label(self, text="Numero de Parte")
-        self.num_parte.grid(row=1, column=0, columnspan=3)
+        self.num_parte.grid(row=2, column=0, columnspan=3)
+
+        self.status = ttk.Label(
+            self, text="Escane la caja para empesar.", borderwidth=1)
+        self.status.grid(row=4, column=0, columnspan=6)
+
+        self.spacer = ttk.Label(self, text="")
+        self.spacer.grid(row=1, column=0, columnspan=6)
+        self.spacer2 = ttk.Label(self, text="")
+        self.spacer2.grid(row=3, column=0, columnspan=6)
 
         # Keys
         self.captura.bind('<Return>', self.captureItem)
@@ -36,24 +45,40 @@ class Application(ttk.Frame):
 
         if self.scan_type == "box":
             np = codigo[-10:]
+            print(np)
             if np in cn.np_codes:
-                self.num_parte.config(text=np)
+                self.num_parte.config(text=cn.codes[np])
                 self.current_np = np
+                self.scan_type = "piece"
+                self.status.config(text="Escanee la Pieza",)
         else:
-            pz = codigo[2:13]
-            pz.replace("-", "")
+            pz = codigo[1:13]
+            pz = pz.replace("-", "")
+            print(pz)
             if pz == self.current_np:
                 self.pz_number += 1
-
-                self.np_number.config(text=str(self.pz_number))
+                if self.pz_number == 10:
+                    self.pz_number = 0
+                    self.scan_type = "box"
+                    self.np_number.config(text=str(self.pz_number))
+                    self.num_parte.config(text="Numero de Parte.")
+                    self.status.config(
+                        text="Escane la caja para empesar.", bootstyle="default")
+                else:
+                    self.np_number.config(text=str(self.pz_number))
+                    self.status.config(
+                        text="Pieza OK.", bootstyle="inverse success")
+            else:
+                self.status.config(
+                    text="Numero de Parte Equivocado.", bootstyle="inverse danger")
 
 
 if __name__ == "__main__":
 
     app = ttk.Window(
-        title="Calculator",
+        title="Verificador De Codigo",
         themename="litera",
-        size=(350, 600),
+        size=(400, 200),
         resizable=(False, False),
     )
     Application(app)
--- src/test.py
+++ src/test.py
@@ -3,4 +3,6 @@ box_code = "3SD95000010036262187786596D"
 piece_code = "P1877865-96-D:1TD9524254009002"
 
 print(box_code[-10:])
-print(piece_code[2:13])
+trimed = piece_code[1:13]
+trimed = trimed.replace("-", "")
+print(trimed)
